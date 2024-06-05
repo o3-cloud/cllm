68457616861ceb3a2ec0c7610f48583a41746a5c
owen.zanzal <ozanzal@gmail.com>
2024-06-05 13:00:18 -0400

    Add RAG chat example script and update .gitignore
    - Updated .gitignore to exclude .cllm/rag directory.
    - Added a new script `chat.sh` under `examples/rag/` for demonstrating a Retrieval-Augmented Generation (RAG) chat application.
      - The script scrapes content from a specified URL and processes it into a RAG store.
      - Implements a simple interactive chat loop that uses the RAG store to provide context-aware responses.
      - Utilizes various `cllm` commands for scraping, splitting, and vectorizing the content.

---
+++ examples/rag/chat.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RAG=promptingguide
+URL="https://www.promptingguide.ai/"
+K=5
+
+RAG_DIR=${CLLM_DIR}/rag/${RAG}
+
+mkdir -p tmp
+
+if [ ! -d "${RAG_DIR}" ]; then
+    echo "Scraper URL: ${URL}"
+    cllm-load-sitemap -u "${URL}" > tmp/${RAG}-sitemap.json
+    cat tmp/${RAG}-sitemap.json | cllm-scraper > tmp/${RAG}-scrape.json
+
+    echo "Adding text to RAG store ${RAG}"
+    cat tmp/${RAG}-scrape.json | cllm-splitter-docs > tmp/${RAG}-split.json
+    cat tmp/${RAG}-split.json | cllm-vector-faiss-write -i "${RAG}"
+fi
+
+
+echo "When done, type 'exit' to quit"
+while true; do
+    read -p "You: " input
+    if [ "$input" == "exit" ]; then
+        break
+    fi
+    echo ""
+    res="$(cllm-vector-faiss-load -k ${K} -i ${RAG} "${input}")"
+    echo "Bot: $(cllm -pc "${res}" -t rag-qa gpt/4o "${input}")"
+    echo ""
+done
