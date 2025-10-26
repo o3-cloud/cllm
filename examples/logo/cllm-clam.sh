2d32a00705d9cb71794657279dcbafb000deee58
owen.zanzal <ozanzal@gmail.com>
2025-10-26 07:14:34 -0400

    reset code base

--- examples/logo/cllm-clam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cllm gpt/4o \
--pr "You are a graphic designer" \
--pc "A logo for an Open Source project" \
--pi "Describe a picture of a cute clam. This clam like to dress like differet types of people." \
--po "A text to image prompt describe. The image should be a simple vector logo. Keep the prompt short and concise." \
--pe "A cute clam dressed like a princess, logo, vector, simple design" | cllm-gen-dalle \
-| jq -r '.[0]' | xargs open
--- examples/ollama/multi-llm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cllm l/llama "Write a story about a dog" | \
-cllm gpt/3.5 "Summerize"
